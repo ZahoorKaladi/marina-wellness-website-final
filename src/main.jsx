555a1d0adb809d5f181fbbc197756413f0d3bcdb
Zahoor Ahmed <82461526+ZahoorKaladi@users.noreply.github.com>
2025-11-08 15:12:58 +0500

    Fix import case for layout component

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import Layout from "./components/Layout";
+import layout from "./components/layout";
 import ScrollToTop from "./components/ScrollToTop";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
